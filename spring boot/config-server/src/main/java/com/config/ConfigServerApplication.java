c5ed44dddf70c9ff36da1fcd8aff1db52f17f332
k <yukai@we.com>
2016-10-17 23:18:05 +0800

    bus

--- spring boot/config-server/src/main/java/com/config/ConfigServerApplication.java
+++ spring boot/config-server/src/main/java/com/config/ConfigServerApplication.java
@@ -1,8 +1,13 @@
 package com.config;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -10,6 +15,15 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+//		Banner banner=new Banner() {
+//			@Override
+//			public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+//				out.print("CONFIG SERVER");
+//			}
+//		};
+//		SpringApplication springApplication=new SpringApplication(ConfigServerApplication.class);
+//		springApplication.setBanner(banner);
+//		springApplication.run(args);
 	}
 
 }
