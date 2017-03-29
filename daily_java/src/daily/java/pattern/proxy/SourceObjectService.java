e418f92443c1a9968291efce918a8b3718c2b2d0
k <yukai@we.com>
2017-03-29 23:58:23 +0800

    to maven project

--- daily_java/src/daily/java/pattern/proxy/SourceObjectService.java
+++
@@ -1,9 +0,0 @@
-package daily.java.pattern.proxy;
-
-/**
- * Created by yukai on 2017/3/29.
- */
-public interface SourceObjectService {
-    public Object getSourceObject();
-    public void saveSourceObject();
-}
