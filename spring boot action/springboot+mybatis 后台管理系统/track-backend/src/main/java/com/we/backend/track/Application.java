add3315044dcb65df0ed306d0f9225a95d34345b
k <yukai@we.com>
2017-08-25 00:28:24 +0800

    springboot jar jsp无法访问

--- spring boot action/springboot+mybatis 后台管理系统/track-backend/src/main/java/com/we/backend/track/Application.java
+++ spring boot action/springboot+mybatis 后台管理系统/track-backend/src/main/java/com/we/backend/track/Application.java
@@ -2,6 +2,7 @@ package com.we.backend.track;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
@@ -13,10 +14,11 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 	
-//	@Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(Application.class);
-//    }
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//            打war包的时候用这个
+        return application.sources(Application.class);
+    }
 
 
 	
