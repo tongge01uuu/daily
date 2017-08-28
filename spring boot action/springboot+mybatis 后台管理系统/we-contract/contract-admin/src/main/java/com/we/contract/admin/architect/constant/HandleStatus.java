3b8f31bf29740376b4474e6d6aeed6ce60fc5f5b
针尖上的舞者 <1185881369@qq.com>
2017-08-28 21:07:57 +0800

    contract

---
+++ spring boot action/springboot+mybatis 后台管理系统/we-contract/contract-admin/src/main/java/com/we/contract/admin/architect/constant/HandleStatus.java
@@ -0,0 +1,35 @@
+package com.we.contract.admin.architect.constant;
+
+/**
+ * Created by yukai on 2017-8-22.
+ */
+public enum HandleStatus {
+    UN_HANDLE(0,"未处理"),
+    HANDLING(1,"处理中"),
+    DONE(2,"已回访");
+
+    private int key;
+    private String description;
+
+    HandleStatus(int key,String description)
+    {
+        this.key=key;
+        this.description=description;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
